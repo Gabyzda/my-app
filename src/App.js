f8cabd6f0386cf255d4a07206fcef6bacf6ec90b
Gabriela Zapata <gabyzda@gmail.com>
2022-07-27 14:05:01 -0300

    BORRADO HEADER DE APP.JS

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editando o componente <code>src/App.js</code> and save to reload.
-        </p>
-      
-      </header>
+   
     </div>
   );
 }
